cd8f8f5e30a3fbc70e6a21980dca60dd60f831d6
supriadiii <supriadiadi1001@gmail.com>
2024-08-29 14:55:28 +0700

    auth user finish

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // Aktifkan transformasi otomatis
+    }),
+  );
+  await app.listen(3002);
+}
+bootstrap();
